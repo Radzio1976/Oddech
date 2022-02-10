c02c9825ff60816444a43a658a0cd81414a679fa
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-02-10 16:33:20 +0100

    Added Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './components/Home';
 
 class App extends React.Component {
   render() {
     return(
-      <h1>Oddech.....</h1>
+      <div id="App">
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
     )
   }
 }
