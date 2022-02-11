3d1cdbbee5f948230e7c496b6fbbd6ef2ca8235f
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-02-11 16:43:21 +0100

    Added Contact component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import Home from './components/Home';
+import Home from './components/Home/Home';
+import Contact from './components/Contact';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Router>
           <Routes>
             <Route path="/" exact element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Router>
       </div>
